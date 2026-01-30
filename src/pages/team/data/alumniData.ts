395ccf717526d7b02deaaaa63baf1621cbdcaae1
avikshitdhar <avikshitdhar75@gmail.com>
2026-01-30 19:08:14 +0530

    Favicon Fix

--- src/pages/team/data/alumniData.ts
+++ src/pages/team/data/alumniData.ts
@@ -16,7 +16,12 @@ export const alumniByYear: AlumniByYear = {
     { name: "Shreya T", designation: "AI Member", image: `${base}alumni/24/shreya.webp` },
     { name: "Venkatesh Durai", designation: "Software Member", image: `${base}alumni/24/venkatesh.jpg` },
     { name: "Sanjana PS", designation: "Software Member", image: `${base}alumni/24/sanjana.webp` },
+    { name: "Shirish Jayaprakash", designation: "DFM Member", image: `${base}alumni/24/shirish.webp` },
+    { name: "Suvan Kamath", designation: "DFM Member", image: `${base}alumni/24/suvan.webp` },
+    { name: "Gautam Gupta", designation: "Software Member", image: `${base}alumni/24/gautam.webp` },
     { name: "Yash Bharadwaj", designation: "Software Member", image: `${base}alumni/24/yash.webp` },
+    { name: "Sankhya Podagatlaballi", designation: "Electronics Member", image: `${base}alumni/24/saankhya.webp` },
+    //Saankhya Podagatlapalli???
   ],
   "2023": [
     { name: "Prakhar Ganeriwal", designation: "Technical Head", image: `${base}alumni/23/prakhar.jpg` },
@@ -24,6 +29,10 @@ export const alumniByYear: AlumniByYear = {
     { name: "Dhruv Patel", designation: "DFM Head", image: `${base}alumni/23/dhruv.jpg` },
     { name: "Anoushka Nag", designation: "AI Head", image: `${base}alumni/23/anoushka.webp` },
     { name: "Nirvana Baid", designation: "Software Head", image: `${base}alumni/23/nirvana.jpg` },
+    { name: "Ruppikha Sree Shankar", designation: "AI Member", image: `${base}alumni/23/ruppikha.jpg` },
+    { name: "Ronan Lobo", designation: "AI Member", image: `${base}alumni/23/ronan.jpg` },
+    { name: "Vatsal Siotia", designation: "Electronics Member", image: `${base}alumni/23/vatsal.jpg` },
+    { name: "Manas Shetty", designation: "Electronics Member", image: `${base}alumni/23/manas.jpg` },
   ],
   "2022": [
     { name: "Shivam", designation: "Team Leader", image: `${base}alumni/22/shivam.jpeg` },
