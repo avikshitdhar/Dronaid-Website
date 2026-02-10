653dfe253196845859c4bc6e028b0776dc2aa8ac
Urja Srivastava <146852383+carcked1t@users.noreply.github.com>
2026-02-10 19:02:56 +0530

    Update Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -33,11 +33,10 @@ const Contact = () => {
 
     emailjs
       .send(
-      import.meta.env.VITE_EMAILJS_SERVICE_ID,
-      import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
-      templateParams
-    );
-
+        import.meta.env.VITE_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+        templateParams
+      )
       .then(() => {
         setSubmitted(true);
         setFormData({ name: '', email: '', subject: '', message: '' });
